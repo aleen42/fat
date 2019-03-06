e2d9b63ed4dfbdd190ff08234696b3912912cd7b
Aleen <aleen42@vip.qq.com>
2019-03-07 00:31:35 +0800

    stage

--- server.js
+++ server.js
@@ -14,48 +14,48 @@ const config = require('./webpack.config.js');
 const fs = require('fs');
 const input = fs.readFileSync('./data.dot', {encoding: 'utf8'});
 
-console.log('Start to render...');
+console.log('Start to parsing...');
 config.plugins.push(new webpack.DefinePlugin({
-    'DOT_INPUT': JSON.stringify(input),
+    'DOT_INPUT': JSON.stringify(require('graphlib-dot').read(input)),
 }));
 
-const app = express();
-const devMiddleWare = require('webpack-dev-middleware')(webpack(config), {
-    publicPath: config.output.publicPath,
-    stats: {
-        colors: true,
-        hash: false,
-        version: true,
-        builtAt: false,
-        timings: true,
-        assets: true,
-        chunks: false,
-        modules: false,
-        reasons: false,
-        children: false,
-        source: false,
-        errors: true,
-        errorDetails: true,
-        warnings: true,
-        publicPath: true,
-    },
-});
-
-devMiddleWare.waitUntilValid(() => {
-    const port = 8080;
-    const url = `http://127.0.0.1:${port}`;
-
-    app.listen(port, function (err) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        const open = process.platform === 'darwin' ? 'open' : (process.platform === 'win32' ? 'start' : 'xdg-open');
-        // open default browser to visit
-        require('child_process').exec(`${open} ${url}`);
-        console.log(`Deployed web at ${url}`);
-    });
-});
-
-app.use(devMiddleWare);
+// const app = express();
+// const devMiddleWare = require('webpack-dev-middleware')(webpack(config), {
+//     publicPath: config.output.publicPath,
+//     stats: {
+//         colors: true,
+//         hash: false,
+//         version: true,
+//         builtAt: false,
+//         timings: true,
+//         assets: true,
+//         chunks: false,
+//         modules: false,
+//         reasons: false,
+//         children: false,
+//         source: false,
+//         errors: true,
+//         errorDetails: true,
+//         warnings: true,
+//         publicPath: true,
+//     },
+// });
+//
+// devMiddleWare.waitUntilValid(() => {
+//     const port = 8080;
+//     const url = `http://127.0.0.1:${port}`;
+//
+//     app.listen(port, function (err) {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//
+//         const open = process.platform === 'darwin' ? 'open' : (process.platform === 'win32' ? 'start' : 'xdg-open');
+//         // open default browser to visit
+//         require('child_process').exec(`${open} ${url}`);
+//         console.log(`Deployed web at ${url}`);
+//     });
+// });
+//
+// app.use(devMiddleWare);
